36873a211b46c946c5b36a1b63fe68f028ec6390
Stalin1824 <name.stalin2003@gmail.com>
2024-08-11 17:00:11 +0530

    updated JS

--- main.js
+++ main.js
@@ -2,16 +2,25 @@ const menuBar = document.querySelector(".bridal-lash-wrapper >div:nth-child(3)")
 const ToSlideLeft = document.querySelector(".bridal-lash-wrapper>nav ")
 const links = document.querySelectorAll(".bridal-lash-wrapper>nav>li>a")
 const mediaQuery = window.matchMedia("(max-width:1200px)")
-
+let a =false
 function handleMediaQuery(e) {
     if (e.matches === true) {
         menuBar.addEventListener("click", () => {
-            ToSlideLeft.style.left = "-5%"
+          a=!a
+          if(a===true){
+           ToSlideLeft.style.left = "-10%"
             ToSlideLeft.style.transition = "0.7s"
+            
+          } else if(a==false){
+               ToSlideLeft.style.left = "-100%"
+                ToSlideLeft.style.transition = "1s"
+            
+          }
+          
         })
         links.forEach((el) => {
             el.addEventListener("click", () => {
-                ToSlideLeft.style.left = "-80%"
+                ToSlideLeft.style.left = "-100%"
                 ToSlideLeft.style.transition = "1s"
 
             })
