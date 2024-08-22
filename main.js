4230e29bddcadb628dd8361db82a745ab4338105
Stalin1824 <name.stalin2003@gmail.com>
2024-08-22 17:13:30 +0530

    Updated file

--- main.js
+++ main.js
@@ -49,67 +49,64 @@ links.forEach((el) => {
 
 
 
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-});
+// window.onload =()=>{
+//     let     parseditem = JSON.parse(localStorage.getItem("package"))   
+//     var  changeable = document.querySelector("#changeable")
+// console.log(parseditem);
 
-
-
-
-
-
-
-
-window.onload =()=>{
-    let     parseditem = JSON.parse(localStorage.getItem("package"))   
-    var  changeable = document.querySelector("#changeable")
-console.log(parseditem);
-
-    let output =""
-    parseditem.forEach((el)=>{
+//     let output =""
+//     parseditem.forEach((el)=>{
                     
-                 output+= 
-                 ` <div  data-aos="zoom-in-up"  data-aos-delay="100"  data-aos-duration="900">
-                                 <div>
-                                     <img width="100%" height="100%"  src=${el.img}   alt="high definition makeup package">
-                                 </div>
-                                 <h3>${el.title}</h3>
-                                 <div>
-                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25" fill="rgba(249,195,82,1)"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM13.5003 8C13.8278 8.43606 14.0625 8.94584 14.175 9.5H16V11H14.175C13.8275 12.7117 12.3142 14 10.5 14H10.3107L14.0303 17.7197L12.9697 18.7803L8 13.8107V12.5H10.5C11.4797 12.5 12.3131 11.8739 12.622 11H8V9.5H12.622C12.3131 8.62611 11.4797 8 10.5 8H8V6.5H16V8H13.5003Z"></path></svg>
-                                     <span  class="price">${el.price}</span>
-                                 </div>
-                                 <button class="addToList">ADD TO LIST</button>
+//                  output+= 
+//                  ` <div  data-aos="zoom-in-up"  data-aos-delay="100"  data-aos-duration="900">
+//                                  <div>
+//                                      <img width="100%" height="100%"  src=${el.img}   alt="high definition makeup package">
+//                                  </div>
+//                                  <h3>${el.title}</h3>
+//                                  <div>
+//                                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25" fill="rgba(249,195,82,1)"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM13.5003 8C13.8278 8.43606 14.0625 8.94584 14.175 9.5H16V11H14.175C13.8275 12.7117 12.3142 14 10.5 14H10.3107L14.0303 17.7197L12.9697 18.7803L8 13.8107V12.5H10.5C11.4797 12.5 12.3131 11.8739 12.622 11H8V9.5H12.622C12.3131 8.62611 11.4797 8 10.5 8H8V6.5H16V8H13.5003Z"></path></svg>
+//                                      <span  class="price">${el.price}</span>
+//                                  </div>
+//                                  <button class="addToList">ADD TO LIST</button>
                                 
-                             </div>`
+//                              </div>`
         
 
-           })    
-                   changeable.innerHTML = output  
+//            })    
+//                    changeable.innerHTML = output  
    
-        let  customize = JSON.parse(localStorage.getItem("customize"))   
-        let output1=''
-        var parsedItem2 = document.querySelector("#changeCustomize")
-        customize.forEach((el)=>{
+//         let  customize = JSON.parse(localStorage.getItem("customize"))   
+//         let output1=''
+//         var parsedItem2 = document.querySelector("#changeCustomize")
+//         customize.forEach((el)=>{
          
-         output1+= 
-         ` <div  data-aos="zoom-in-up"  data-aos-delay="100"  data-aos-duration="900">
-                         <div>
-                             <img width="100%" height="100%"  src=${el.img}   alt="high definition makeup package">
-                         </div>
-                         <h3>${el.title}</h3>
-                         <div>
-                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25" fill="rgba(249,195,82,1)"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM13.5003 8C13.8278 8.43606 14.0625 8.94584 14.175 9.5H16V11H14.175C13.8275 12.7117 12.3142 14 10.5 14H10.3107L14.0303 17.7197L12.9697 18.7803L8 13.8107V12.5H10.5C11.4797 12.5 12.3131 11.8739 12.622 11H8V9.5H12.622C12.3131 8.62611 11.4797 8 10.5 8H8V6.5H16V8H13.5003Z"></path></svg>
-                             <span  class="price">${el.price}</span>
-                         </div>
-                         <button class="addToList">ADD TO LIST</button>
+//          output1+= 
+//          ` <div  data-aos="zoom-in-up"  data-aos-delay="100"  data-aos-duration="900">
+//                          <div>
+//                              <img width="100%" height="100%"  src=${el.img}   alt="high definition makeup package">
+//                          </div>
+//                          <h3>${el.title}</h3>
+//                          <div>
+//                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25" fill="rgba(249,195,82,1)"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM13.5003 8C13.8278 8.43606 14.0625 8.94584 14.175 9.5H16V11H14.175C13.8275 12.7117 12.3142 14 10.5 14H10.3107L14.0303 17.7197L12.9697 18.7803L8 13.8107V12.5H10.5C11.4797 12.5 12.3131 11.8739 12.622 11H8V9.5H12.622C12.3131 8.62611 11.4797 8 10.5 8H8V6.5H16V8H13.5003Z"></path></svg>
+//                              <span  class="price">${el.price}</span>
+//                          </div>
+//                          <button class="addToList">ADD TO LIST</button>
                         
-                     </div>`
+//                      </div>`
 
 
-   })          
-   parsedItem2.innerHTML = output1  
+//    })          
+//    parsedItem2.innerHTML = output1  
                     
-                }
+//                 }
+
+
+
+
+
+
+
+
 
 
 
